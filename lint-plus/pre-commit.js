dd3b04a3a5e436f341561d01297fc87333916c1b
YangJiyuan <yjy972080142@gmail.com>
2016-04-05 16:16:17 +0800

    update lint-plus pre-commit-hook

--- lint-plus/pre-commit.js
+++ lint-plus/pre-commit.js
@@ -1,7 +1,25 @@
 var child_process=require("child_process"),
     spawnSync=child_process.spawnSync;
 
-var result=spawnSync("npm",["test"]);
+var result;
+// 判断是否是master分支
+result=spawnSync("git",["branch","--column"]);
+var branchs = result.stdout.toString().split(' ');
+var len = branchs.length,branch,currentBranch;
+for(var i=0;i<len;i++){
+    branch = branchs[i].trim();
+    if(branch && branch === '*'){
+        currentBranch = branchs[i+1];
+        if(currentBranch){
+            currentBranch = currentBranch.replace(/(^\n)|(\n$)/g,'')
+        }
+        break;
+    }
+}
+if(!currentBranch || currentBranch !== 'master'){
+    process.exit(0);
+}
+result=spawnSync("npm",["test"]);
 if(result.status){
     console.log(result.stdout.toString());
     console.log('Commit aborted.Test failed.See above for more details');
