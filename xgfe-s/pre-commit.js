efbe0b66f83394cb8f028183176f1f90112b49d3
陈五百 <chenwubai.cx@gmail.com>
2016-04-14 11:12:06 +0800

    fix(xgfe-s): quit when lintFiles.length == 0

--- xgfe-s/pre-commit.js
+++ xgfe-s/pre-commit.js
@@ -28,6 +28,9 @@ var lintFiles = files.filter(function (file) {
 }).map(function (file) {
     return file.subpath;
 });
+if(!lintFiles.length){
+    quit();
+}
 var argv = ['lint'];
 argv = argv.concat(lintFiles);
 argv = argv.concat(['-c','src/.lintrc']);
